a6c0ef6bc066e03ef20de501f6fd3c2b9df06560
wkarts <57051272+wkarts@users.noreply.github.com>
2024-07-31 14:59:46 -0300

    Update GenerateStatics.sh

--- WAProto/GenerateStatics.sh
+++ WAProto/GenerateStatics.sh
@@ -1,11 +1,11 @@
-# Instalar protobufjs e protobufjs-cli globalmente
-yarn global add protobufjs protobufjs-cli
+# Instalar protobufjs e protobufjs-cli localmente
+yarn add protobufjs protobufjs-cli
 
-# Adicionar a localização dos binários globalmente instalados ao PATH
-export PATH="$(yarn global bin):$PATH"
+# Adicionar a localização dos binários localmente instalados ao PATH
+export PATH="./node_modules/.bin:$PATH"
 
 # Gerar os arquivos estáticos do protobuf
-yarn pbjs -t static-module -w commonjs -o ./WAProto/index.js ./WAProto/WAProto.proto;
-yarn pbts -o ./WAProto/index.d.ts ./WAProto/index.js;
+./node_modules/.bin/pbjs -t static-module -w commonjs -o ./WAProto/index.js ./WAProto/WAProto.proto
+./node_modules/.bin/pbts -o ./WAProto/index.d.ts ./WAProto/index.js
 
 #protoc --plugin=./node_modules/.bin/protoc-gen-ts_proto --ts_proto_opt=env=node,useOptionals=true,forceLong=long --ts_proto_out=. ./src/Binary/WAMessage.proto;
